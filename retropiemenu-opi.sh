13721318f36349cca19b3537bf07877737929b4d
microplay-hub <123579956+microplay-hub@users.noreply.github.com>
2023-02-04 13:58:29 +0100

    fix 1

--- retropiemenu-opi.sh
+++ retropiemenu-opi.sh
@@ -41,10 +41,10 @@ function install_retropiemenu-opi() {
 	
     cd "$md_inst"
 	
-	cp -r "rpmenuicons.sh" "$rpmsetup/retropiemenu-opi.sh"
-    chown -R $user:$user "$rpmsetup/retropiemenu-opi"
-	chmod 755 "$rpmsetup/retropiemenu-opi"
-	rm -r "retropiemenu-opi"
+	cp -r "retropiemenu-opi.sh" "$rpmsetup/retropiemenu-opi.sh"
+    chown -R $user:$user "$rpmsetup/retropiemenu-opi.sh"
+	chmod 755 "$rpmsetup/retropiemenu-opi.sh"
+	rm -r "retropiemenu-opi.sh"
 	
 }
 
